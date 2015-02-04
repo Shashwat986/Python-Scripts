1711111156c6e83a9b0cbf56208cb3d6966ca07e
Shashwat Chandra <chandra.shashwat@gmail.com>
2015-02-04 09:04:53 +0530

    adding customization to class definition. Still ongoing.

--- cards.py
+++ cards.py
@@ -1,9 +1,11 @@
+import copy
 import random
+
 values = [0,'A','2','3','4','5','6','7','8','9','10','J','Q','K']
 suits = ['C','D','H','S']
 joker = ['0','0']
 
-class Card:
+class Card(object):
 	rank = '0'
 	suit = '0'
 	value = 0
@@ -16,21 +18,25 @@ class Card:
 		
 		return values.index(self.rank)
 
-class Deck:
+class Deck(object):
 	def __init__(self, numDecks=1, jflag=0):
-		self.cards = [(v,s) for v in values[1:] for s in suits]*numDecks
+		self.cards = [Card(v,s) for v in values[1:] for s in suits for deck in range(numDecks)]
+		self.numDecks = numDecks
+		self.jflag = jflag
 		if jflag:
 			 self.cards += joker*jflag
-		self.orig_cards=self.cards
+		self.orig_cards=copy.deepcopy(self.cards)
 		random.shuffle(self.cards)
 	def draw(self):
-		crd = self.cards.pop()
-		card = Card(crd[0],crd[1])
+		if self.deckSize ():
+			card = self.cards.pop()
+		else:
+			raise IndexError("Deck Empty!")
 		return card
 	def deckSize(self):
 		return len(self.cards)
 	def reshuffle(self):
-		self.cards = self.orig_cards
+		self.cards = copy.deepcopy(self.orig_cards)
 		random.shuffle(self.cards)
 	def shuffle(self):
 		random.shuffle(self.cards)
@@ -49,4 +55,17 @@ class Deck:
 		while(self.deckSize()):
 			dealt[ctr%n].append(self.draw())
 			ctr+=1
-		return deck
+		return dealt
+	def __len__(self):
+		return self.deckSize()
+	def __getitem__(self,key):
+		pass
+	def __reversed__(self):
+		pass
+	def __iter(self):
+		pass
+	def __repr__(self):
+		return "<{} {} with {} {} remaining>".format(self.numDecks, \
+			"deck" if self.numDecks == 1 else "decks", \
+			self.deckSize(), \
+			"card" if self.deckSize() == 1 else "cards")
